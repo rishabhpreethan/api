84f0c45ec5f3f5f63bea43bb9d1ee609856ce948
rishabhpreethan <rishabh.preethan@gmail.com>
2023-02-21 20:10:49 +0530

    committed

---
+++ url_shortener/main.py
@@ -0,0 +1,14 @@
+import requests
+
+def shorten_link(full_link,link_name):
+    base_url="https://cutt.ly/api/api.php"
+    api="51de7be2f870ac3d5b7159aaf2707a28e3bf3"
+    p = {'key':api,"short":full_link,"name":link_name}
+    response=requests.get(base_url,params=p).json()
+    # print(response)
+    return response['url']['shortLink']
+
+full_link=input("Enter a link that you want to shorten\n")
+link_name=input("Enter the name\n")
+# shorten_link("https://gogoanime.co.in/","gogo")
+print(shorten_link(full_link,link_name))
